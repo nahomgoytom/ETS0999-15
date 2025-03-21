e4cb1178078a325d2f96398816f38a0e62df17fb
nahomgoytom <nahom.goytom@aastustudent.edu.et>
2025-03-21 11:26:28 +0300

    Update Isupper.py

--- Nahom G/String_Method/Isupper()/Isupper.py
+++ Nahom G/String_Method/Isupper()/Isupper.py
@@ -1,1 +1,9 @@
+N = "DORM"
+N2 = "dorm"
+N3 = "123!"
+N4 = "DORM4"
 
+print(N.isupper())  # True (All uppercase)
+print(N1.isupper())  # False (Contains lowercase letters)
+print(N2.isupper())  # False (No uppercase letters)
+print(N3.isupper())  # True (Numbers don't affect the result)
