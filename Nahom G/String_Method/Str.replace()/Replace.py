7e60443db46eb3922b2c92f966d881e9f65c5ce2
nahomgoytom <nahom.goytom@aastustudent.edu.et>
2025-03-25 20:36:41 +0300

    Create Replace.py

---
+++ Nahom G/String_Method/Str.replace()/Replace.py
@@ -0,0 +1,6 @@
+T = "I hate candies. candies are too sweet."
+
+# Replace "candies" with "cakes"
+new_text = text.replace("candies", "cakes")
+
+print(new_text)  # Output: "I hate cakes. cakes are too sweet."
